b4c56e5c2514725facea3bdc055f0241b073eee9
René Højbjerg Larsen <rhl@fynskemedier.dk>
2019-07-22 16:36:02 +0200

    Use simplified trusted proxy setup for JFM #OP-6

--- web/app.php
+++ web/app.php
@@ -11,15 +11,7 @@ include_once __DIR__.'/../app/bootstrap.php.cache';
 $kernel = new AppKernel('prod', false);
 $kernel->loadClassCache();
 
-if ($_SERVER['REMOTE_ADDR'] === '144.217.203.53') {
-    Request::setTrustedProxies([$_SERVER['REMOTE_ADDR']]);
-    // force all trusted header names
-    Request::setTrustedHeaderName(Request::HEADER_FORWARDED, '');
-    Request::setTrustedHeaderName(Request::HEADER_CLIENT_IP, 'X_REAL_IP');
-    Request::setTrustedHeaderName(Request::HEADER_CLIENT_HOST, '');
-    Request::setTrustedHeaderName(Request::HEADER_CLIENT_PROTO, '');
-    Request::setTrustedHeaderName(Request::HEADER_CLIENT_PORT, '');
-}
+Request::setTrustedProxies(['10.254.0.73','10.254.0.74']);
 
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
